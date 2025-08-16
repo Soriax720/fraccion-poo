fb7e3f38e9077318f750955cb79610ccadb567e1
Soriax720 <julisoria744@gmail.com>
2025-08-16 13:52:17 -0300

    a method is added to operate on the sum of fractions

--- fraccion/clas1.py
+++ fraccion/clas1.py
@@ -2,7 +2,10 @@ class Fraccion(object):
     def __init__(self,numerador,denominador):
         self.numerador = numerador
         self.denominador = denominador
-    
+    def __add__(self, other):
+        numerador = self.numerador + other.numerador
+        denominador = self.denominador
+        return Fraccion(numerador,denominador)
 
 
 
