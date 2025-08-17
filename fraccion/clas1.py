1395f4f79e0403d5d4b69cb7e285a5dba9c6491d
Soriax720 <julisoria744@gmail.com>
2025-08-17 17:33:14 -0300

    add division method

--- fraccion/clas1.py
+++ fraccion/clas1.py
@@ -2,24 +2,31 @@ class Fraccion(object):
     def __init__(self,numerador,denominador):
         self.numerador = numerador
         self.denominador = denominador
+
     def __add__(self, other):
         numerador = self.numerador + other.numerador
         denominador = self.denominador
         return Fraccion(numerador,denominador)
+    
     def __str__(self):
         return f"{self.numerador}/{self.denominador}"
+    
     def __sub__(self, other):
         numerador = self.numerador - other.numerador
         denominador = self.denominador
         return Fraccion(numerador,denominador)
+    
     def __mul__(self, other):
         numerador = self.numerador * other.numerador
         denominador = self.denominador
         return Fraccion(numerador,denominador)
-
+    
+    def __truediv__(self,other):
+        numerador = self.numerador / other.numerador
+        denominador = self.denominador
+        return Fraccion(numerador,denominador)
 """
-
+f = Fraccion(10,5)
+e = Fraccion(5,5)
+print(f / e)
 """
-f = Fraccion(2,3)
-e = Fraccion(5,3)
-print(f * e)
